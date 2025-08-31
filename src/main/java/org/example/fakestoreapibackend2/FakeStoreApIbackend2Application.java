44dfd8e2e17e382a214ee17135d56a94909de919
Honey Bergstrom <phueng03735@gmail.com>
2025-08-31 22:22:12 +0200

    initial project structure with DB config

---
+++ src/main/java/org/example/fakestoreapibackend2/FakeStoreApIbackend2Application.java
@@ -0,0 +1,13 @@
+package org.example.fakestoreapibackend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStoreApIbackend2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeStoreApIbackend2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fakestoreapibackend2/FakeStoreApIbackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fakestoreapibackend2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStoreApIbackend2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
